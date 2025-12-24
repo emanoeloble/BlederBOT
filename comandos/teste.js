55cd1577efd8a1d329a083b5dda95560782cf82a
emanoeloble <emanoelantony384@gmail.com>
2025-12-24 14:49:49 -0300

    Update teste.js

--- comandos/teste.js
+++ comandos/teste.js
@@ -1,1 +1,7 @@
+const Discord = require("discord.js");
 
+exports.run = async (client, interaction) => {
+return await interaction.reply({content: 'O comando está funcionando!'}).catch(err => {
+return console.log(err)
+})
+}
